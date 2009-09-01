aeda16299a55db20ab2945e5751c419a02627961
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2009-09-01 09:08:59 +0000

    Tilføjet concrete5 template til autonav for menuer i venstre sidebar.

---
+++ blocks/autonav/templates/kita_left_nav.php
@@ -0,0 +1,41 @@
+<?php 
+	defined('C5_EXECUTE') or die(_("Access Denied."));
+	$aBlocks = $controller->generateNav();
+	global $c;
+	echo("<ul class=\"leftNav\">");
+
+	$nh = Loader::helper('navigation');
+
+	$isFirst = true;
+	foreach($aBlocks as $ni) {
+		$_c = $ni->getCollectionObject();
+		if (!$_c->getCollectionAttributeValue('exclude_nav')) {
+			
+			$pageLink = false;
+
+			if ($_c->getCollectionAttributeValue('replace_link_with_first_in_nav')) {
+				$subPage = $_c->getFirstChild();
+				if ($subPage instanceof Page) {
+					$pageLink = $nh->getLinkToCollection($subPage);
+				}
+			}
+			
+			if (!$pageLink) {
+				$pageLink = $ni->getURL();
+			}
+			
+			echo '<li>';
+			
+			if ($c->getCollectionID() == $_c->getCollectionID()) { 
+				echo('<a class="current" href="' . $pageLink . '">' . $ni->getName() . '</a>');
+			} else {
+				echo('<a href="' . $pageLink . '">' . $ni->getName() . '</a>');
+			}	
+			
+			echo('</li>');
+			$isFirst = false;			
+		}
+	}
+	
+	echo('</ul>');
+?>
