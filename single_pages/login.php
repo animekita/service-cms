f73d0e1a0e053ecf3e36ce0d8aacc053971357f6
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2009-10-09 22:15:25 +0000

    Rettet sti til selvbetjening fra concrete login siden.

--- single_pages/login.php
+++ single_pages/login.php
@@ -1,5 +1,5 @@
 <?php  defined('C5_EXECUTE') or die(_("Access Denied."));
 
-header('Location: http://selvbetjening.anime-kita.dk/profil/login/?next=' . $_SERVER['REQUEST_URI'], true, 303);
+header('Location: http://www.anime-kita.dk/selvbetjening/profil/login/?next=' . $_SERVER['REQUEST_URI'], true, 303);
 exit();
 
