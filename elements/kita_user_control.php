f68b9372ef45977a5a699d4a0f23ae09dd2c7b92
sema <sema@cb2309eb-351f-4c53-ad2e-ab06ce95a7ee>
2010-01-19 22:20:00 +0000

    Tilføjet config for selvbetjening url for C5.

--- elements/kita_user_control.php
+++ elements/kita_user_control.php
@@ -3,12 +3,12 @@
 <div id="topUserNav">
 <!-- This is why whitespaces inside block elements shouldn't be parsed -->
 <?php $u = new User(); if ($u->isRegistered()) { ?>
-<a class="floatLeft" href="https://selvbetjening.anime-kita.dk/profil/"><?php echo $u->uName; ?></a
+<a class="floatLeft" href="<?php echo SELV_URL; ?>/profil/"><?php echo $u->uName; ?></a
 ><span class="vertSeperatorTiny">|</span
-><a href="https://selvbetjening.anime-kita.dk/profil/logud/">Log ud</a>
+><a href="<?php echo SELV_URL; ?>/profil/logud/">Log ud</a>
 <?php } else { ?>
-<a class="floatLeft" href="https://selvbetjening.anime-kita.dk/bliv-medlem/">Opret Bruger</a
+<a class="floatLeft" href="<?php echo SELV_URL; ?>/bliv-medlem/">Opret Bruger</a
 ><span class="vertSeperatorTiny">|</span
-><a href="https://selvbetjening.anime-kita.dk/profil/login/?next=<?php  echo $_SERVER['REQUEST_URI']; ?>">Log ind</a>
+><a href="<?php echo SELV_URL; ?>/profil/login/?next=<?php  echo $_SERVER['REQUEST_URI']; ?>">Log ind</a>
 <?php } ?>
 </div>
